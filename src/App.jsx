149e55737e5f69cc4d2909be7b08557cb1224ac1
Izzy-100 <israasakhakhni@gmail.com>
2025-05-17 09:41:45 -0700

    Add CreateRecipeButton component and create a responsive layout for a one line centered theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import Navigation from './components/Navigation/Navigation.jsx';
 import ImageLink from './components/ImageLink/ImageLink.jsx';
 import IngredientList from './components/IngredientList/IngredientList.jsx';
+import CreateRecipeButton from './components/CreateRecipeButton/CreateRecipeButton.jsx';
+
 import './App.css';
 import 'tachyons';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <ImageLink />
-      <IngredientList />
-      {/* 
-      <ItemRecognition />
-      <Recipe />
-      <CreateButton /> */}
+      <Navigation/>
+      <div className='container1'>
+        <div className='container2'>
+          <CreateRecipeButton/>        
+          <ImageLink/>
+        </div>
+        <IngredientList/>
+      </div>
+      {/*
+      <ItemRecognition/>
+      <Recipe/>
+       */}
     </div>
   )
 }
