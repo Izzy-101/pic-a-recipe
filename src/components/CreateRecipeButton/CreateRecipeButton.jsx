9613138670fe76a52e6c6be63f756c11639687d5
Izzy-100 <israasakhakhni@gmail.com>
2025-05-17 09:29:41 -0700

    Build CreateRecipeButton component for recipe generation

---
+++ src/components/CreateRecipeButton/CreateRecipeButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CreateRecipeButton.css';
+
+const CreateRecipeButton = () => {
+    return (
+        <div className='h-20'>
+            <button className='button-84 grow link'>Create Recipes</button>
+        </div>
+    );
+}
+
+export default CreateRecipeButton;
