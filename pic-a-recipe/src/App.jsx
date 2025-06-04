6fa3761b5800504b64ff8bc6eb86054c5fdc7103
Izzy-100 <israasakhakhni@gmail.com>
2025-06-04 09:45:56 -0700

    Lift imgUrl and dataIngredient state for shared access across components. Finalize responsive layout with FoodImage component.

--- pic-a-recipe/src/App.jsx
+++ pic-a-recipe/src/App.jsx
@@ -2,25 +2,30 @@ import Navigation from './components/Navigation/Navigation.jsx';
 import ImageLink from './components/ImageLink/ImageLink.jsx';
 import IngredientList from './components/IngredientList/IngredientList.jsx';
 import CreateRecipeButton from './components/CreateRecipeButton/CreateRecipeButton.jsx';
+import FoodImage from './components/FoodImage/FoodImage.jsx';
+
+import {useState, useEffect} from 'react';
 
 import './App.css';
 import 'tachyons';
 
 function App() {
+  const [imgUrl, setImgUrl] = useState('');
+  const [dataIngredient, setDataIng] = useState([]);
+  
   return (
     <div className="App">
       <Navigation/>
       <div className='container1'>
         <div className='container2'>
           <CreateRecipeButton/>        
-          <ImageLink/>
+          <ImageLink setImgUrl={setImgUrl} imgUrl={imgUrl} setDataIng={setDataIng} dataIngredient={dataIngredient}/>
         </div>
-        <IngredientList/>
+        <div className='size-shift'>
+          <IngredientList dataIngredient={dataIngredient}/>
+        </div>
+        <FoodImage className='food-image' imgUrl={imgUrl} />
       </div>
-      {/*
-      <ItemRecognition/>
-      <Recipe/>
-       */}
     </div>
   )
 }
