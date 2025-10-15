bd95f9852518e1bf7a51a484c211abd37a92671b
Izzy-100 <israasakhakhni@gmail.com>
2025-10-15 08:13:02 -0700

    feat: fetch recipe info by ID using Spoonacular API and extract single recipe to update dataRecipe state

--- pic-a-recipe/src/components/CreateRecipeButton/CreateRecipeButton.jsx
+++ pic-a-recipe/src/components/CreateRecipeButton/CreateRecipeButton.jsx
@@ -1,16 +1,56 @@
 import './CreateRecipeButton.css';
+import {useEffect} from 'react';
 
-const CreateRecipeButton = ({dataIngredient}) => {
+const CreateRecipeButton = ({dataIngredient, dataRecipe, setDataRecipe}) => {
     const KEY = import.meta.env.VITE_KEY;
     const ingredients = dataIngredient.join(',');
 
+    useEffect(() => {
+        console.log('Updated dataRecipe:', dataRecipe);
+    }, [dataRecipe]); 
+
+    //fetch recipe by ingredients and setDataRecipe
     const handleButtonClick = async () => {
-        try {
+        try { 
             const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=10&ranking=1&ignorePantry=true&apiKey=${KEY}`)
-            const data = await response.json();
-            console.log('API Response:', data, 'ingredients:', ingredients);
+            const recipes = await response.json();
+            const response2 = await fetch(`https://api.spoonacular.com/recipes/${recipes[0].id}/information?includeNutrition=true&apiKey=${KEY}`)
+            const recipeData = await response2.json();
+
+            const finalDataRecipe = [];
+
+            finalDataRecipe.push({
+                id: recipeData.id,
+                title: recipeData.title,
+                image: recipeData.image,
+                info: {
+                    prep: recipeData.preparationMinutes === null ? '' : recipeData.preparationMinutes,
+                    total: recipeData.readyInMinutes === null ? '' : recipeData.readyInMinutes,
+                    cook: recipeData.cookingMinutes === null ? '' : recipeData.cookingMinutes,
+                    serving: recipeData.servings === null ? '' : recipeData.servings
+                },
+                nutrition: {
+                    calories: recipeData.nutrition.nutrients[0].amount,
+                    fat: recipeData.nutrition.nutrients[1].amount,
+                    carbohydrates: recipeData.nutrition.nutrients[3].amount,
+                    sugar: recipeData.nutrition.nutrients[5].amount,
+                    protein: recipeData.nutrition.nutrients[10].amount,
+                    fiber: recipeData.nutrition.nutrients[18].amount
+                },
+                ingredients: {
+                    ing: recipeData.extendedIngredients.map(ing => {
+                        return ing.original;
+                    })
+                },
+                instructions: { 
+                    steps: recipeData.instructions === null ? '' : recipeData.instructions
+                },
+                summary: recipeData.summary
+            });
+        
+            setDataRecipe(finalDataRecipe);
         } catch(error) {
-            console.log('Error calling the API:', error);
+            console.log('Error handleButtonClick:', error);
         }
     }
     return (
