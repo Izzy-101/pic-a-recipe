046210bb322359932b3fa3634717023b8c895b21
Izzy-100 <israasakhakhni@gmail.com>
2025-10-15 08:05:01 -0700

    feat: fetch recipes by ingredients using Spoonacular API

--- pic-a-recipe/src/components/CreateRecipeButton/CreateRecipeButton.jsx
+++ pic-a-recipe/src/components/CreateRecipeButton/CreateRecipeButton.jsx
@@ -1,9 +1,21 @@
 import './CreateRecipeButton.css';
 
-const CreateRecipeButton = () => {
+const CreateRecipeButton = ({dataIngredient}) => {
+    const KEY = import.meta.env.VITE_KEY;
+    const ingredients = dataIngredient.join(',');
+
+    const handleButtonClick = async () => {
+        try {
+            const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=10&ranking=1&ignorePantry=true&apiKey=${KEY}`)
+            const data = await response.json();
+            console.log('API Response:', data, 'ingredients:', ingredients);
+        } catch(error) {
+            console.log('Error calling the API:', error);
+        }
+    }
     return (
         <div className='h-20'>
-            <button className='button-84 grow link'>Create Recipes</button>
+            <button onClick={handleButtonClick} className='button-84 grow link'>Create Recipes</button>
         </div>
     );
 }
