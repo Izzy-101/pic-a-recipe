66a3eacdbe0adcf95c4dc79d428cd5fb0179dc20
Izzy-100 <israasakhakhni@gmail.com>
2025-10-20 12:07:51 -0700

    feat: filter out recipes missing instructions or ingredients before rendering

--- pic-a-recipe/src/components/RecipeBook/RecipeBook.jsx
+++ pic-a-recipe/src/components/RecipeBook/RecipeBook.jsx
@@ -5,11 +5,15 @@ import CoverPage from '../CoverPage/CoverPage';
 import 'tachyons';
 
 const RecipeBook = ({dataRecipe}) => {
+    //filter out recipes missing instructions or ingredients
+    const validateRecipes = (recipes) => {
+        return (recipes.instructions.steps.length !== 0 && recipes.ingredients.ing.length !== 0)    
+    }
     return (
         <div className="center ma4">
             <HTMLFlipBook showCover={true} width={450} height={650} maxShadowOpacity={0.5}>
                 <CoverPage title="My Recipe Book"></CoverPage>
-                {dataRecipe.map((recipe, i) => {
+                {dataRecipe.filter(validateRecipes).map((recipe, i) => {
                     return <RecipePage key={recipe.id} recipe={recipe} number={i+1}>Page text</RecipePage>
                 })}
             </HTMLFlipBook>
