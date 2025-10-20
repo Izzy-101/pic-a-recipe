785f291b43fe6c5617c0a02ba26cffc6c3cba8e5
Izzy-100 <israasakhakhni@gmail.com>
2025-10-20 12:06:09 -0700

    feat: add RecipeBook component and recipe state handling

--- pic-a-recipe/src/App.jsx
+++ pic-a-recipe/src/App.jsx
@@ -3,6 +3,7 @@ import ImageLink from './components/ImageLink/ImageLink.jsx';
 import IngredientList from './components/IngredientList/IngredientList.jsx';
 import CreateRecipeButton from './components/CreateRecipeButton/CreateRecipeButton.jsx';
 import FoodImage from './components/FoodImage/FoodImage.jsx';
+import RecipeBook from './components/RecipeBook/RecipeBook.jsx';
 
 import {useState, useEffect} from 'react';
 
@@ -12,20 +13,22 @@ import 'tachyons';
 function App() {
   const [imgUrl, setImgUrl] = useState('');
   const [dataIngredient, setDataIng] = useState([]);
-  
+  const [dataRecipe, setDataRecipe] = useState([]);
+
   return (
     <div className="App">
       <Navigation/>
       <div className='container1'>
         <div className='container2'>
-          <CreateRecipeButton/>        
+          <CreateRecipeButton dataRecipe={dataRecipe} setDataRecipe={setDataRecipe} dataIngredient={dataIngredient}/>        
           <ImageLink setImgUrl={setImgUrl} imgUrl={imgUrl} setDataIng={setDataIng} dataIngredient={dataIngredient}/>
         </div>
         <div className='size-shift'>
-          <IngredientList dataIngredient={dataIngredient}/>
+          <IngredientList dataIngredient={dataIngredient} setDataIng={setDataIng}/>
         </div>
         <FoodImage className='food-image' imgUrl={imgUrl} />
       </div>
+      <RecipeBook dataRecipe={dataRecipe} />
     </div>
   )
 }
